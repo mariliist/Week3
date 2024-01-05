1911665cbca91c69ff2ea78e4c183823206fecc8
Mariliis Toon <mariliistoon@gmail.com>
2024-01-05 18:27:45 +0200

    hindelised

---
+++ Week3Hindeline/Program.cs
@@ -0,0 +1,38 @@
+﻿//programm küsib, mis hinde kasutaja sai
+//kui kasutaja sai 'A', konsool kuvab "Suurepärane!"; jne
+
+Console.WriteLine("Mis hinde sa said?");
+
+string userGrade = Console.ReadLine().ToUpper();
+
+if(userGrade == "A")
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if(userGrade == "B")
+{
+    Console.WriteLine("Väga hea!");
+}
+else if(userGrade == "C")
+{
+    Console.WriteLine("Hea!");
+}
+else if(userGrade == "D")
+{
+    Console.WriteLine("Rahuldav!");
+}
+else if(userGrade == "E")
+{
+    Console.WriteLine("Kasin!");
+}
+else if(userGrade == "F")
+{
+    Console.WriteLine("Pudulik!");
+}
+else
+{
+    Console.WriteLine("Vale väärtus!");
+}
+
+
+
---
+++ Week3HindelineSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Mis hinde sa said?");
+
+string userGrade = Console.ReadLine().ToUpper();
+
+switch(userGrade) 
+
+{
+    case "A":
+
+        Console.WriteLine("Suurepärane!");
+        break;
+
+    case "B":
+
+        Console.WriteLine("Väga hea!");
+        break;
+    case "C":
+
+        Console.WriteLine("Hea!");
+        break;
+    case "D":
+
+        Console.WriteLine("Rahuldav!");
+        break;
+    case "E":
+
+        Console.WriteLine("Kasin!");
+        break;
+    case "F":
+
+        Console.WriteLine("Pudulik!");
+        break;
+
+    default:
+        Console.WriteLine("Vale väärtus!");
+        break;
+    }
